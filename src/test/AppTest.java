7ef47a6bca814467186c0c2129f1a103694ae450
Zakaria El Yousfi <zakariaelyousfi99@gmail.com>
2022-01-20 02:49:03 +0100

    Aspects fonctionel Q2 Test

--- src/test/AppTest.java
+++ src/test/AppTest.java
@@ -25,23 +25,41 @@ public class AppTest {
     public void testAspectFunctionalOne() {
 
         Continent continent = new Continent("Europe","eur");
-        Country  country=  new Country("france","fr","EUR","SALAM",continent);
+        Country  country=  new Country("france","FR","EUR","SALAM",continent);
 
 
         countryDAO.addNewCountry(country);
 
-        Country actualCountry = countryDAO.getByCode("fr");
+        Country actualCountry = countryDAO.getByCode("FR");
          System.out.println(actualCountry.getCode());
+
         assertEquals(country.getCode(),actualCountry.getCode());
 
         assertEquals(country.getName(),actualCountry.getName());
         assertEquals(country.getDevise(),actualCountry.getDevise());
         assertEquals(country.getGreetings(),actualCountry.getGreetings());
-      assertEquals(country.getContinent().getCode(),actualCountry.getContinent().getCode());
-       assertEquals(country.getContinent().getName(),actualCountry.getContinent().getName());
+        assertEquals(country.getContinent().getCode(),actualCountry.getContinent().getCode());
+        assertEquals(country.getContinent().getName(),actualCountry.getContinent().getName());
 
     }
 
+    @Test
+    public void testAspectFunctionalTwo() {
+
+        Continent continent = new Continent("Europe","eur");
+        Country  country=  new Country("Spain","es","EURO","Hola",continent);
+
+        Country actualCountry = countryDAO.getByCode("es");
+
+
+        assertEquals(country.getCode(),actualCountry.getCode());
+        assertEquals(country.getName(),actualCountry.getName());
+        assertEquals(country.getDevise(),actualCountry.getDevise());
+        assertEquals(country.getGreetings(),actualCountry.getGreetings());
+        assertEquals(country.getContinent().getCode(),actualCountry.getContinent().getCode());
+        assertEquals(country.getContinent().getName(),actualCountry.getContinent().getName());
+
+    }
 
 
 }
